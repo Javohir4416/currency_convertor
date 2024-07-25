368b559c541a72a1925d168875759937d2fcd1a6
Javohir <javohirfoziljonov2002@gmail.com>
2024-07-25 13:45:35 +0500

    adding dependencies , creating database and enable feign

---
+++ src/main/java/javokhir/dev/currency_convertor/constants/RestConstants.java
@@ -0,0 +1,11 @@
+package javokhir.dev.currency_convertor.constants;
+
+import java.util.logging.Logger;
+
+public interface RestConstants {
+    String TELEGRAM_BASE_URL="https://api.telegram.org";
+    String BOT_TOKEN="5076755668:AAGcJDCFYryIsTCt78sQy7g-T-bfBiyd5sA";
+    String PASSWORD="menman";
+    static Logger logger = Logger.getLogger("Logger");
+
+}
---
+++ src/main/java/javokhir/dev/currency_convertor/feign/TelegramFeign.java
@@ -0,0 +1,16 @@
+package javokhir.dev.currency_convertor.feign;
+import javokhir.dev.currency_convertor.component.payload.ResultTelegram;
+import javokhir.dev.currency_convertor.constants.RestConstants;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+@FeignClient(url = RestConstants.TELEGRAM_BASE_URL,name = "TelegramFeign")
+public interface TelegramFeign {
+
+
+
+    @PostMapping("/bot"+RestConstants.BOT_TOKEN+"/sendMessage")
+    ResultTelegram sendMessageToUser(@RequestBody SendMessage sendMessage);
+}
